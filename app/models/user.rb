d3f500a6b6a9b8affb8902428dcfe2315d327879
rodeofly <florian.tobe@gmail.com>
2016-01-19 14:43:43 +0400

    remove semi-colo :(

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   
   enum sexes: {'sexe' => nil, 'garçon' => 1, 'fille' => 2}
   enum ages:  {'age' => nil, '- de 6 ans' => 6, '7 ans'=> 7, '8 ans'=> 8, '9 ans'=> 9, '10 ans'=> 10, '11 ans'=> 11, '12 ans'=> 12, '13 ans'=> 13, '14 ans'=> 14, '15 ans'=> 15, '16 ans et +'=> 16 }
-  enum classes: {'classe' => nil,'primaire'=> 12, 'cp'=> 11, 'ce1'=> 10, 'ce2'=> 9, 'cm1'=> 8, 'cm2'=> 7, '6ème'=> 6, '5ème'=> 5, '4ème'=> 4, '3ème'=> 3, 'Lycée'=> 2, 'ailleurs':1 }
+  enum classes: {'classe' => nil,'primaire'=> 12, 'cp'=> 11, 'ce1'=> 10, 'ce2'=> 9, 'cm1'=> 8, 'cm2'=> 7, '6ème'=> 6, '5ème'=> 5, '4ème'=> 4, '3ème'=> 3, 'Lycée'=> 2, 'ailleurs' => 1 }
   enum locations: {'lieu' => nil, 'Ile de la Réunion'=> 1, 'France Métropole'=> 2, 'Autre'=> 3}   
 
 end
