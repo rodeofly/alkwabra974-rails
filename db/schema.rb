71a697ad5b28defd5b6bd26a9b68365f4c377a59
rodeo_fly <florian.tobe@gmail.com>
2016-01-26 13:07:18 +0400

    update tentatives_count

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126082531) do
+ActiveRecord::Schema.define(version: 20160126082532) do
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
