c837974166f340ed2a90e07daa6f8a62035a45d4
rodeo_fly <florian.tobe@gmail.com>
2016-02-07 15:27:05 +0400

    restore admin auth

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -8,7 +8,11 @@ RailsAdmin.config do |config|
   end
   # config.current_user_method(&:current_user)
   
-  
+  config.authorize_with do
+    puts "#########################################################################"
+    puts current_user.admin?
+    redirect_to main_app.root_path unless current_user.admin?
+  end
   ## == Cancan ==
   # config.authorize_with :cancan
 
