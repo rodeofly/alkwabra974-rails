b8d8e19ab3636e6207902d31ef6a33d31d66a2ea
rodeo_fly <florian.tobe@gmail.com>
2016-02-07 13:43:05 +0400

    azerty

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :deploy_to, '/home/deploy/alkwabra974-rails'
 set :rbenv_path, '/home/deploy/.rbenv'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
