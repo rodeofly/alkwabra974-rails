841364060f51e6e6559f030302167d1a7a98ebb7
rodeo_fly <florian.tobe@gmail.com>
2016-02-20 11:05:07 +0400

    rails_admin tentatives list config

--- app/models/tentative.rb
+++ app/models/tentative.rb
@@ -1,4 +1,17 @@
 class Tentative < ActiveRecord::Base
   belongs_to :user, counter_cache: :tentatives_count
   belongs_to :game
+  
+  rails_admin do
+    list do
+      field :id
+      field :user
+      field :chapter
+      field :level
+      field :shots
+      field :beurk
+      field :score 
+    end
+  end
+  
 end
